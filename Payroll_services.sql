9d70c9016cced2ea013eaa782600bafcc724441f
unknown <sivaranjanib99@gmail.com>
2021-07-31 07:57:32 +0530

    [siva]UC2 creating a table

--- Payroll_services.sql
+++ Payroll_services.sql
@@ -1,3 +1,12 @@
-******UC!*******
+******UC1*******
 create database payroll_services;
 use payroll_services;
+******UC2*******
+create table employee_payroll
+(
+id int identity(1,1) primary key,
+name varchar(200) not null,
+salary float,
+startDate date
+)
+
