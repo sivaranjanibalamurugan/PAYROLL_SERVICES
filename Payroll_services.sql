b63a7ac7e905bb7c7e8d689bef44c2133e4b2be2
unknown <sivaranjanib99@gmail.com>
2021-07-31 12:34:22 +0530

    [siva]UC7 finding the sum min max and avg salary

--- Payroll_services.sql
+++ Payroll_services.sql
@@ -10,7 +10,8 @@ salary float,
 startDate date
 )
 *********UC3**********
-insert into employee_payroll(name,startDate,salary)values('siva','2021-07-31',34344,'M'),('Dhoni','2020-07-31',243518,'M');
+insert into employee_payroll values('siva','2021-07-31',34344,'M'),('Dhoni','2020-07-31',243518,'M');
+
 **********uc4*********
 select*from employee_payroll;
 **********uc5*********
@@ -18,6 +19,11 @@ select salary,name from employee_payroll where name = 'siva';
 select * from employee_payroll where startDate between cast('2020-07-31' as date) and getdate();
 **********UC6*********
 alter table employee_payroll add gender char(1);
-update employee_payroll set gender='M';
-
+update employee_payroll set gender='M' where name='Dhoni';
+update employee_payroll set gender='F' where name='siva';
+**********UC7**********
+select sum(salary) as totalSalary,gender from employee_payroll group by  gender;
+select avg(salary) as avgSalary,gender from employee_payroll group by  gender;
+select min(salary) as minSalary,gender from employee_payroll group by  gender;
+select max(salary) as maxSalary,gender from employee_payroll group by  gender;
 
