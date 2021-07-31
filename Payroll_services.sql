e89a50d41889a360de419be9cb3327b73b08aea0
unknown <sivaranjanib99@gmail.com>
2021-07-31 08:29:38 +0530

    [siva]UC3 instering the row in the payroll

--- Payroll_services.sql
+++ Payroll_services.sql
@@ -9,4 +9,7 @@ name varchar(200) not null,
 salary float,
 startDate date
 )
+*********UC3**********
+insert into employee_payroll(name,startDate,salary)values('siva','2021-07-31',34344),('Dhoni','2020-07-31',243518);
+select*from employee_payroll;
 
