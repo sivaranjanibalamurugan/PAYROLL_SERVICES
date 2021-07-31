28a56ac832cc3a425bef435ccf22422f59f95360
unknown <sivaranjanib99@gmail.com>
2021-07-31 08:58:22 +0530

    [siva]Uc5 retrieving a particular person salary

--- Payroll_services.sql
+++ Payroll_services.sql
@@ -11,5 +11,9 @@ startDate date
 )
 *********UC3**********
 insert into employee_payroll(name,startDate,salary)values('siva','2021-07-31',34344),('Dhoni','2020-07-31',243518);
+**********uc4*********
 select*from employee_payroll;
+**********uc5*********
+select salary,name from employee_payroll where name = 'siva';
+select * from employee_payroll where startDate between cast('2020-01-01' as date) and getdate();
 
