f01a8534bef57cc8fff9c23d5b801f6c49767d5b
unknown <sivaranjanib99@gmail.com>
2021-07-31 09:32:47 +0530

    [siva]UC6 Adding the gender column in the table

--- Payroll_services.sql
+++ Payroll_services.sql
@@ -10,7 +10,7 @@ salary float,
 startDate date
 )
 *********UC3**********
-insert into employee_payroll(name,startDate,salary)values('siva','2021-07-31',34344),('Dhoni','2020-07-31',243518);
+insert into employee_payroll(name,startDate,salary)values('siva','2021-07-31',34344,'M'),('Dhoni','2020-07-31',243518,'M');
 **********uc4*********
 select*from employee_payroll;
 **********uc5*********
@@ -18,4 +18,6 @@ select salary,name from employee_payroll where name = 'siva';
 select * from employee_payroll where startDate between cast('2020-07-31' as date) and getdate();
 **********UC6*********
 alter table employee_payroll add gender char(1);
+update employee_payroll set gender='M';
+
 
